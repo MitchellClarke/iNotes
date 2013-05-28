d2b1a6bb66ac77e306a7585c603b3d86d7a29590
Mitchell Clarke <mtc5197@psu.edu>
2013-05-28 10:28:01 -0400

    Real Init

---
+++ NotesApp/app.js
@@ -0,0 +1,21 @@
+﻿Ext.application({
+    name: "NotesApp",
+    controllers: ["Notes"],
+    views: ["NotesList","NotesListContainer", "NoteEditor"],
+	stores: ["Notes"],
+	models: ["Note"],
+	requires: ["Ext.Toolbar"],
+    launch: function () {
+
+        var notesListContainer = 
+		{
+			xtype: "noteslistcontainer"
+		};
+		var noteEditor =
+		{
+			xtype: "noteeditor"
+        };
+        Ext.Viewport.add(notesListContainer);
+		Ext.Viewport.add(noteEditor);
+    }
+});
